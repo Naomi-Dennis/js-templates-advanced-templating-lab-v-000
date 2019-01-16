b75d069aa10c41cf15ae516872b22a54b3dbfcfd
Naomi Dennis <njdennis94@gmail.com>
2019-01-16 00:03:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function init() {
 
 function displayEditForm(){
   clearForm('#show-page')
-  document.querySelector("main").innerHTML += recipe_db[recipe_db.length - 1].form
+  addToPage(recipe_db[recipe_db.length - 1].form)
 }
 function createRecipe(){
     
@@ -42,34 +42,40 @@ function displayIngredientHandler(ingredient){
 }
 
 function addToPage(html){
-  document.body.innerHTML += html 
+  let complete_html = {"data": undefined, "fn":undefined, "html": undefined}
+  complete_html.data = html 
+  document.querySelector("main").innerHTML = complete_html.html
+  
 }
 function clearForm(query){
   let ele = document.querySelector(query)
+  if(ele){
   ele.parentNode.removeChild(ele)
+  }
 }
 
 function handleSubmit(){
   let recipe_form = document.getElementById("recipe-form")
- 
-  let form_details = recipe_form.elements 
-  recipe_form.querySelector("#name").setAttribute("value", form_details["name"].value)
-  recipe_form.querySelector("#description").setAttribute("value", form_details["description"].value)
+  if(recipe_form){
+  let recipe_form_name =  recipe_form.querySelector("#name")
+  let recipe_form_description = recipe_form.querySelector("#description")
+//  let form_details = recipe_form.elements 
+  recipe_form_name.setAttribute("value", recipe_form_name.value)
+  recipe_form_description.setAttribute("value", recipe_form_description.value)
   
   for(let ingredient of recipe_form.querySelectorAll("input[name='ingredients']")){
     ingredient.setAttribute("value", ingredient.value)
   }
   let ingredients_list = document.getElementsByName("ingredients")
-  let new_recipe = new Recipe(recipe_form, form_details["name"], form_details["description"], ingredients_list)
-  showPage();
+  let new_recipe = new Recipe(recipe_form, recipe_form_name, recipe_form_description, ingredients_list)
+  }
+  showPage()
 }
-
 function showPage(){
-  clearForm("form");
+    clearForm("form");
    let recipe_details_partial_data = document.getElementById("recipe-template").innerHTML 
    let recipe_details_partial_context = recipe_db[0].getDetails()
-   
-   let recipe_details_partial_fn = Handlebars.compile(recipe_details_partial_data)
+    let recipe_details_partial_fn = Handlebars.compile(recipe_details_partial_data)
    let recipe_details_html = recipe_details_partial_fn(recipe_details_partial_context)
-   addToPage(recipe_details_html)
+  addToPage(recipe_details_html)
 }
